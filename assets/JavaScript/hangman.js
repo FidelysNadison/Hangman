dd53d0a0e2efad349a18d991f68107ea77f24995
Nadison <nadisonsolojeanfidelys@gmail.com>
2022-03-10 15:29:22 +0100

    dernier changement

---
+++ assets/JavaScript/hangman.js
@@ -0,0 +1,8 @@
+const wordOption = ['php','css','javascript','html','java','python','react',];
+const word = document.getElementById('word');
+const falseWord =document.getElementById('wrongWord');
+const trueWord = document.getElementById('rightWord');
+const stickman = document.getElementById('stickman');
+
+
+
